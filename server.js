67caf70fd64f723988b65de304a288e08db7f009
batman <2advancesolutions@gmail.com>
2023-01-02 06:47:49 -0600

    create express app and project setup

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+
+// Create Server
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+const { v4: uuidV4 } = require('uuid');
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+// create a new room dynamtically 
+app.get('/', (req, res) => {
+    res.redirect(`/${uuidV4()}`)
+});
+app.get('/:room', (req, res) => {
+    res.render('room', { roomId: req.params.room });
+});
+
+
+// start server
+server.listen(3000);
+
+
